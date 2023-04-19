9e9d05c1f1fcaa362f9343c3b805f842481efc68
Alieu Friggeri <afrigger@c2r1s6.42lausanne.ch>
2023-04-19 14:08:39 +0200

    cpp01 finished

--- cpp01/ex05/srcs/Harl.cpp
+++ cpp01/ex05/srcs/Harl.cpp
@@ -9,18 +9,15 @@ Harl::~Harl(void){
 }
 
 void Harl::debug(void){
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-\
-	ketchup burger. I really do !" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do !" << std::endl;
 }
 
 void Harl::info(void){
-	std::cout << " cannot believe adding extra bacon costs more money. You didn’t put\
-	enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << std::endl;
+	std::cout << " cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << std::endl;
 }
 
 void Harl::warning(void){
-	std::cout << " think I deserve to have some extra bacon for free. I’ve been coming\
-	for years whereas you started working here since last month" << std::endl;
+	std::cout << " think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month" << std::endl;
 }
 
 void Harl::error(void){
@@ -28,5 +25,27 @@ void Harl::error(void){
 }
 
 void Harl::complain(std::string level){
-	std::cout << level << std::endl;
+	
+	std::string arr[4] = {
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"
+	};
+
+	void (Harl::*functionsptr[4])() = {
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
+	};
+	for (int i = 0; i < 4; i++)
+	{
+		if (arr[i].compare(level) == 0)
+		{
+			(this->*functionsptr[i])();
+			return;
+		}
+	}
+	std::cout << "Invalid argument, usage: <program> (DEBUG/INFO/WARNING/ERROR)" << std::endl;
 }
--- cpp01/ex05/srcs/main.cpp
+++ cpp01/ex05/srcs/main.cpp
@@ -1,10 +1,13 @@
 #include "../headers/Harl.hpp"
 
 int main(int ac, char **av){
-
+	
 	Harl harl;
-	(void)ac;
-	(void)av;
-	harl.complain("lol");
+	if (ac != 2 || !av)
+	{
+		std::cout << "Argument cannot be eempty, usage: <program> (DEBUG/INFO/WARNING/ERROR)" << std::endl;
+		return (1);
+	}
+	harl.complain(av[1]);
 	return (0);
 }
